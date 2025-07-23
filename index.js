b64c6b753c4a00f7ab0214eb557b1fc4ef27b891
Kwaku Tutu <65915855+KwakuTutu@users.noreply.github.com>
2025-07-23 02:10:37 +0000

    Portfolio website version 2
    - Created index.html with header, hero section, projects, about, and contact sections.
    - Added JavaScript for navigation toggle and smooth scrolling.
    - Implemented CSS styles for layout, typography, and responsive design.
    - Included SVG icons for social media and design tools.
    - Established a color scheme and typography for the site.

---
+++ index.js
@@ -0,0 +1,69 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+
+navToggle.addEventListener('click', () =>{
+        nav.classList.toggle('nav--visible');
+})
+
+function toggleMenu() {
+        const navLinks = document.querySelector('.nav');
+        const hamburger = document.querySelector('.hamburger');
+      
+        navLinks.classList.toggle('show'); // <-- Fix here
+        hamburger.classList.toggle('close');
+}
+function closeMenu() {
+        const navVisible = document.querySelector('.nav--visible');
+        const navLinks = document.querySelector('.links');
+        const hamburger = document.querySelector('.hamburger');
+      
+        navLinks.classList.remove('show');
+        hamburger.classList.remove('close');
+        navVisible.classList.remove('nav--visible');
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+        const links = document.querySelectorAll('.links');
+      
+        links.forEach(link => {
+          link.addEventListener('click', function(event) {
+            const href = this.getAttribute('href');
+            
+            if (href && href.startsWith('#')) {
+              // Handle scrolling for internal anchor links
+              event.preventDefault();
+              const target = document.querySelector(href);
+              const headerOffset = 30;
+              const elementPosition = target.getBoundingClientRect().top;
+              const offsetPosition = elementPosition - headerOffset;
+              // Scroll to the target element with custom scroll behavior
+              window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+              });
+            }
+          });
+        });
+      });
+      
+
+
+
+
+      document.addEventListener('DOMContentLoaded', function() {
+        const sections = document.querySelectorAll('.section-animate');
+      
+        function checkInView() {
+          sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+      
+            if (sectionTop < windowHeight - 100) {
+              section.classList.add('animate');
+            }
+          });
+        }
+      
+        window.addEventListener('scroll', checkInView);
+      });
